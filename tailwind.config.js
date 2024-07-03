7ecbb7fbcd421f5422f64d2dee4c7ea4a45e6b0d
itzmesojib <itzmesojib@gmail.com>
2024-07-03 13:54:50 +0600

    some edit on tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,14 @@
 import flowbite from "flowbite-react/tailwind";
 module.exports = {
   content: [
-    flowbite.content(),
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+      
     },
   },
   plugins: [
