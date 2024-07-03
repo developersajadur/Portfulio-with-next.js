9ba3083efe1513671a29a6e790fd2e692b21303a
itzmesojib <itzmesojib@gmail.com>
2024-07-03 14:21:40 +0600

    backprevius file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-const flowbite = require('flowbite/plugin');
-
+import flowbite from "flowbite-react/tailwind";
 module.exports = {
   content: [
-    './node_modules/flowbite-react/**/*.js',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    flowbite.content(),
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [
-    flowbite,
+    flowbite.plugin(),
   ],
 };
