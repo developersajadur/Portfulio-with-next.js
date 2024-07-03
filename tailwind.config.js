faf27f90878f76afad3d4c614b14cfea0d0230f6
itzmesojib <itzmesojib@gmail.com>
2024-07-03 13:44:48 +0600

    back the previus tailwind config file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
-const flowbite = require('flowbite/plugin');
-
+import flowbite from "flowbite-react/tailwind";
 module.exports = {
   content: [
-    './node_modules/flowbite/**/*.js',
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    flowbite.content(),
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
   plugins: [
-    flowbite,
+    flowbite.plugin(),
   ],
 };
