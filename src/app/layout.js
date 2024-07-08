5a6da4c693078b172ee3ad70c18202b83681e540
itzmesojib <itzmesojib@gmail.com>
2024-07-08 12:25:32 +0600

    add mouse cursor style

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,11 +2,12 @@ import { Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/Components/Shared/Navbar";
 import Footer from "@/Components/Shared/Footer";
+import AnimatedCursor from "react-animated-cursor";
 
 const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Sajadur Rahman | Welcome to My Universe | Full-Stack Web Developer | React.js & Next.js Specialist | WordPress Expert | Transforming Ideas Into Digital Reality",
+  title: "Sajadur Rahman | Full-Stack Web Developer | React.js & Next.js Specialist | WordPress Expert | Transforming Ideas Into Digital Reality",
   description: "Welcome to the professional portfolio of Sajadur Rahman, a full-stack web developer specializing in React.js and Next.js. As a WordPress expert, I transform ideas into digital reality. Explore my projects, skills, and expertise in creating dynamic, user-friendly web applications and websites.",
   keywords: "Sajadur Rahman, Full-Stack Web Developer, React.js & Next.js Specialist, WordPress Expert, Transforming Ideas Into Digital Reality, Portfolio, Projects, Skills, Experience",
 };
@@ -15,6 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={spaceGrotesk.className}>
+      <AnimatedCursor
+     color="28, 100, 242" 
+       />
         <div className="">
         <Navbar />
         {children}
